7e4eaf5fc6ec227d89fab7a383ba7c45c5491d02
Blitu82 <garpaues@yahoo.es>
2023-11-01 15:15:59 +0000

    added capitals array, randomCapital and distance

---
+++ src/capitals.js
@@ -0,0 +1,1 @@
+{"type":"FeatureCollection","features":[{"type":"Feature","properties":{"name":"Lisbon"},"geometry":{"coordinates":[-9.137048664483302,38.72115737483867],"type":"Point"},"id":0},{"type":"Feature","properties":{"name":"Madrid"},"geometry":{"coordinates":[-3.703353323315355,40.416927815983826],"type":"Point"},"id":1}]}
